230017f9667cd98303ad808503307e6217f6d2cc
abellfalow-nackademin <abell.falow@yh.nackademin.se>
2026-09-25 17:21:38 +0200

    Added props to MovieCard.jsx and api call in Dashboard.jsx

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,14 +1,14 @@
 import "../styles/movieCard.css"
 import { Link } from "react-router"
 
-function MovieCard() {
+function MovieCard(movie) {
 
   return (
-    <Link to={"/"} className="movie-card">
-      <img src="https://image.tmdb.org/t/p/w500/1E5baAaEse26fej7uHcjOgEE2t2.jpg" alt="" />
+    <Link to={`/movie/${movie.id}`} className="movie-card">
+      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
       <div className="rating-card">
         <span className="star-symbol">✦</span>
-        <p className="rating">5.7</p>
+        <p className="rating">{movie.vote_average.toFixed(1)}</p>
       </div>
     </Link>
   )
--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,16 +1,38 @@
 import MovieCard from "../components/MovieCard"
+import { useEffect, useState } from "react";
 
 function DashboardPage() {
 
+  const [movies, setMovies] = useState([]);
+
+  const options = {
+    method: 'GET',
+    headers: {accept: 'application/json', Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhNDE3ZmQyZjg4YzVhMjMyNzVkZWJhMTc1ZjNkNmIwMCIsIm5iZiI6MTc2ODg1Mjk2NS4zNTUsInN1YiI6IjY5NmU4ZGU1YmE1MTE3YzM3ZGY5NTRmYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BE62T0Abjfbrq9JknHONcT-YuWE8BSBUpQK4m1v4FRw'}
+  };
+
+  useEffect(() => {
+
+    const getData = async () => {
+      try {
+        const res = await fetch('https://api.themoviedb.org/3/movie/popular?language=en-US&page=1', options);
+        const data = await res.json();
+        setMovies(data.results)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+
+    getData();
+
+  }, [])
+
   return (
     <div className="movie-grid">
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
-      <MovieCard/>
+      {
+        movies.map(movie => (
+          <MovieCard key={movie.id} id={movie.id} poster_path={movie.poster_path} vote_average={movie.vote_average}/>
+        ))
+      }
     </div>
   )
 }
